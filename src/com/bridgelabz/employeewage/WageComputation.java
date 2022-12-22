478531ca7e8f27871ef47629ad450170e36e7e49
pranit67 <pranit67@gmail.com>
2022-12-22 23:04:26 +0530

    ADDED: code for Employee Wages For Month

--- src/com/bridgelabz/employeewage/WageComputation.java
+++ src/com/bridgelabz/employeewage/WageComputation.java
@@ -4,20 +4,26 @@ public class WageComputation {
     public static void main(String[] args) {
         final int WAGE_PER_HR = 20;
         final int FULL_DAY_HR = 8;
-        int dailyWage;
+        int dailyWage = 0;
+        int month = 0;
         int partTimeHR = 4;
-        double number = Math.floor(Math.random() * 10) % 3;
-        switch ((int) number) {
-            case 1:
-                dailyWage = WAGE_PER_HR * FULL_DAY_HR;
-                System.out.println("Employee is Full Time And Per Day Wage is " + dailyWage + " Rs.");
-                break;
-            case 2:
-                dailyWage = WAGE_PER_HR * partTimeHR;
-                System.out.println("Employee is Part Time And Per Day Wage is " + dailyWage + " Rs.");
-                break;
-            default:
-                System.out.println("Employee is Absent");
+        for (int i = 1; i <= 20; i++) {
+            double number = Math.floor(Math.random() * 10) % 3;
+            switch ((int) number) {
+                case 1:
+                    dailyWage = WAGE_PER_HR * FULL_DAY_HR;
+                    System.out.println("Day " + i + " Employee is Full Time And Per Day Wage is " + dailyWage + " Rs.");
+                    break;
+                case 2:
+                    dailyWage = WAGE_PER_HR * partTimeHR;
+                    System.out.println("Day " + i + " Employee is Part Time And Per Day Wage is " + dailyWage + " Rs.");
+                    break;
+                default:
+                    System.out.println("Day " + i + " Employee is Absent");
+            }
+            month = month + dailyWage;
         }
+        System.out.println("-------------------------------------------------");
+        System.out.println("Overall Monthly Wages Of Employee is " + month);
     }
 }
