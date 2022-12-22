8f50aa99cd5eaeeed001bcee3bb5eca09b4fb4ad
pranit67 <pranit67@gmail.com>
2022-12-22 18:25:46 +0530

    ADDED: code for Employee Wages Using Switch

--- src/com/bridgelabz/employeewage/WageComputation.java
+++ src/com/bridgelabz/employeewage/WageComputation.java
@@ -7,14 +7,17 @@ public class WageComputation {
         int dailyWage;
         int partTimeHR = 4;
         double number = Math.floor(Math.random() * 10) % 3;
-        if (number == 1) {
-            dailyWage = WAGE_PER_HR * FULL_DAY_HR;
-            System.out.println("Employee is Full Time And Per Day Wage is " + dailyWage + " Rs.");
-        } else if (number == 2) {
-            dailyWage = WAGE_PER_HR * partTimeHR;
-            System.out.println("Employee is Part Time And Per Day Wage is " + dailyWage + " Rs.");
-        } else {
-            System.out.println("Employee is Absent");
+        switch ((int) number) {
+            case 1:
+                dailyWage = WAGE_PER_HR * FULL_DAY_HR;
+                System.out.println("Employee is Full Time And Per Day Wage is " + dailyWage + " Rs.");
+                break;
+            case 2:
+                dailyWage = WAGE_PER_HR * partTimeHR;
+                System.out.println("Employee is Part Time And Per Day Wage is " + dailyWage + " Rs.");
+                break;
+            default:
+                System.out.println("Employee is Absent");
         }
     }
 }
