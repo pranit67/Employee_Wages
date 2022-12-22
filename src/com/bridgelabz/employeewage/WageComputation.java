43a3bc04323d3bbf3051f88982364387f5a0f807
pranit67 <pranit67@gmail.com>
2022-12-22 18:00:49 +0530

    ADDED: code for Daily Employee Wages

--- src/com/bridgelabz/employeewage/WageComputation.java
+++ src/com/bridgelabz/employeewage/WageComputation.java
@@ -2,10 +2,13 @@ package com.bridgelabz.employeewage;
 
 public class WageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+        final int WAGE_PER_HR = 20;
+        final int FULL_DAY_HR = 8;
+        int dailyWage = 0;
         double number = Math.floor(Math.random() * 10) % 2;
         if (number == 1) {
-            System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HR * FULL_DAY_HR;
+            System.out.println("Employee is Present And Per Day Wage is " + dailyWage +" Rs.");
         } else {
             System.out.println("Employee is Absent");
         }
