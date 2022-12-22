6b127a1536e059362d7a12161b7f536a7fa0ac87
pranit67 <pranit67@gmail.com>
2022-12-22 18:16:11 +0530

    ADDED: code for Part Time Employee Wages

--- src/com/bridgelabz/employeewage/WageComputation.java
+++ src/com/bridgelabz/employeewage/WageComputation.java
@@ -4,11 +4,15 @@ public class WageComputation {
     public static void main(String[] args) {
         final int WAGE_PER_HR = 20;
         final int FULL_DAY_HR = 8;
-        int dailyWage = 0;
-        double number = Math.floor(Math.random() * 10) % 2;
+        int dailyWage;
+        int partTimeHR = 4;
+        double number = Math.floor(Math.random() * 10) % 3;
         if (number == 1) {
             dailyWage = WAGE_PER_HR * FULL_DAY_HR;
-            System.out.println("Employee is Present And Per Day Wage is " + dailyWage +" Rs.");
+            System.out.println("Employee is Full Time And Per Day Wage is " + dailyWage + " Rs.");
+        } else if (number == 2) {
+            dailyWage = WAGE_PER_HR * partTimeHR;
+            System.out.println("Employee is Part Time And Per Day Wage is " + dailyWage + " Rs.");
         } else {
             System.out.println("Employee is Absent");
         }
