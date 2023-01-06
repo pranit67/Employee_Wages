793329712c9f5a49e25fcc866a40c442aa74a5bb
amitatiwari255 <amita.tiwari255@gmail.com>
2023-01-06 16:15:23 +0530

    ADDED: Refactor Code by using Class and Object

--- src/com/bridgelabz/employeewage/WageComputation.java
+++ src/com/bridgelabz/employeewage/WageComputation.java
@@ -1,15 +1,17 @@
 package com.bridgelabz.employeewage;
 
 public class WageComputation {
-    public static void main(String[] args) {
-        final int WAGE_PER_HR = 20;
-        final int WORKING_DAYS = 20;
-        int max_emp_hr = 100;
-        int dailyWage;
-        int empHr;
-        int totalEmpWage;
-        int totalEmpHr = 0;
-        int total_working_days = 1;
+    final int WAGE_PER_HR = 20;
+    final int WORKING_DAYS = 20;
+    int max_emp_hr = 100;
+    int dailyWage;
+    int empHr;
+    int totalEmpWage;
+    int totalEmpHr = 0;
+    int total_working_days = 1;
+
+    void wageCalculation() {
+
         while (totalEmpHr <= max_emp_hr && total_working_days <= WORKING_DAYS) {
             double number = Math.floor(Math.random() * 10) % 3;
             switch ((int) number) {
@@ -31,5 +33,11 @@ public class WageComputation {
         System.out.println("-------------------------------------------------");
         totalEmpWage = totalEmpHr * WAGE_PER_HR;
         System.out.println("Monthly wage is " + totalEmpWage);
+    }
+
+
+    public static void main(String[] args) {
+        WageComputation object = new WageComputation();
+        object.wageCalculation();
     }
 }
