64a540a4d258616f00b4ad450a519d02d323788f
pranit67 <pranit67@gmail.com>
2023-02-06 18:18:44 +0530

    Added:code for Multiple Companies

--- src/com/bridgelabz/employeewage/WageComputation.java
+++ src/com/bridgelabz/employeewage/WageComputation.java
@@ -1,42 +1,58 @@
 package com.bridgelabz.employeewage;
 
 public class WageComputation {
-    final int WAGE_PER_HR = 20;
-    final int WORKING_DAYS = 20;
-    int max_emp_hr = 100;
-    int dailyWage;
-    int empHr;
-    int totalEmpWage;
-    int totalEmpHr = 0;
-    int total_working_days = 1;
+    int emp_hr = 0;
+    int dailyWage = 0;
+    static int totalEmpWage = 0;
+    int total_emp_hr = 0;
+    int total_working_days = 0;
+    int empCheck = 0;
+    int numOfPresentFullDay = 0;
+    int numOfPresentHalfDay = 0;
+    int numOfAbsentDays = 0;
 
-    void wageCalculation() {
-
-        while (totalEmpHr <= max_emp_hr && total_working_days <= WORKING_DAYS) {
-            double number = Math.floor(Math.random() * 10) % 3;
-            switch ((int) number) {
+    public void emplpoyeeWage(int WAGE_PER_HR, int WORKING_DAYS, int max_emp_hr, String companyName) {
+        System.out.println("Employee wages for the company " + companyName + " is :- ");
+        while (total_emp_hr <= max_emp_hr && total_working_days < WORKING_DAYS) {
+            empCheck = (int) (Math.floor((Math.random() * 10)) % 3);
+            switch (empCheck) {
                 case 1:
-                    empHr = 8;
-                    dailyWage = WAGE_PER_HR * empHr;
+                    emp_hr = 8;
+                    numOfPresentFullDay++;
                     break;
                 case 2:
-                    empHr = 4;
-                    dailyWage = WAGE_PER_HR * empHr;
+                    emp_hr = 4;
+                    numOfPresentHalfDay++;
                     break;
-                default:
-                    empHr = 0;
+                case 0:
+                    emp_hr = 0;
+                    numOfAbsentDays++;
+                    break;
             }
-            totalEmpHr = totalEmpHr + empHr;
-            System.out.println("Day " + total_working_days + " Total working hrs " + totalEmpHr);
-            total_working_days++;
+            WORKING_DAYS--;
+            total_emp_hr = total_emp_hr + emp_hr;
         }
-        System.out.println("-------------------------------------------------");
-        totalEmpWage = totalEmpHr * WAGE_PER_HR;
-        System.out.println("Monthly wage is " + totalEmpWage);
+        System.out.println("Number of working days full day :- " + numOfPresentFullDay);
+        System.out.println("Number of working days half day :- " + numOfPresentHalfDay);
+        System.out.println("Number of Absent Days :- " + numOfAbsentDays);
+        totalEmpWage = total_emp_hr * WAGE_PER_HR;
     }
-
     public static void main(String[] args) {
-        WageComputation object = new WageComputation();
-        object.wageCalculation();
+
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+        int WAGE_PER_HR;
+        int WORKING_DAYS;
+        int max_emp_hr;
+        String companyName;
+
+        WageComputation obj = new WageComputation();
+        obj.emplpoyeeWage(10, 4, 20, "Telco");
+        System.out.println("Monthly wage of Employee is " + totalEmpWage);
+
+        System.out.println("------------------------------------------------------------------------------------");
+
+        obj.emplpoyeeWage(20, 2, 10, "Emcure");
+        System.out.println("Monthly wage of Employee is " + totalEmpWage);
     }
 }
